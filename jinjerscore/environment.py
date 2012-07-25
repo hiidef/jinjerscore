0bfad195bcd5baf4f9920eb48df0307f9ef9f5f8
Brent Hagany <brent@hiidef.com>
2012-07-25 18:45:18 -0500

    Automatically enable the extension in the jinjerscore environment

--- jinjerscore/environment.py
+++ jinjerscore/environment.py
@@ -1,10 +1,18 @@
 from jinja2.environment import Environment
 from jinja2.utils import _encode_filename
 from jinjerscore.compiler import generate
+from jinjerscore.ext import JinjerscoreExtension
 from jinjerscore.parser import JinjerscoreParser
 
 
 class JinjerscoreEnvironment(Environment):
+    def __init__(self, *args, **kwargs):
+        extensions = kwargs.get('extensions', [])
+        extensions += [JinjerscoreExtension]
+        kwargs['extensions'] = extensions
+        super(JinjerscoreEnvironment, self).__init__(*args, **kwargs)
+        self.generate_underscore = True
+
     def _parse(self, source, name, filename):
         return JinjerscoreParser(self, source, name, _encode_filename(filename)).parse()
 
